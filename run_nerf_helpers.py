645bcc4c19a675e953bed9050aa708132f03a2b0
xiazhi1 <2194625680@qq.com>
2023-10-12 14:52:45 +0800

    total frame but have bug

--- run_nerf_helpers.py
+++ run_nerf_helpers.py
@@ -5,7 +5,7 @@ import numpy as np
 
 
 # misc
-img2mse=lambda x,y:torch.mean((x-y)**2)
+img2mse=lambda x, y :torch.mean((x-y) ** 2)
 mse2psnr=lambda x:-10.*torch.log(x)/torch.log(torch.Tensor([10.]))
 to8b=lambda x:(255*np.clip(x,0,1)).astype(np.uint8)
 
@@ -78,10 +78,10 @@ class NeRF(nn.Module):
         self.skips=skips # skips is used to decide where to add the input_ch
         self.use_viewdirs=use_viewdirs
 
-        # the ML{'s design  mainly refers the Fig7 of the origin paper
+        # the MLP's design  mainly refers the Fig7 of the origin paper
 
         self.pts_linears=nn.ModuleList(
-            [nn.Linear(input_ch,W)]+[nn.Linear(W,W) if i is not in self.skips else nn.Linear(W+input_ch,W) for i in range(D-1)]
+            [nn.Linear(input_ch,W)]+[nn.Linear(W,W) if i not in self.skips else nn.Linear(W+input_ch,W) for i in range(D-1)]
         ) # define 0-7 layer and concat in the fifth layer
 
         self.view_linears=nn.ModuleList([nn.Linear(input_ch_views+W,W//2)])
@@ -94,27 +94,29 @@ class NeRF(nn.Module):
             self.output_linear=nn.Linear(W,output_ch)
 
     def forward(self,x):
-        input_pts,input_views=torch.split(x,[self.input_ch,self.input_ch_views],-1)
+        input_pts,input_views=torch.split(x,[self.input_ch,self.input_ch_views],dim=-1)
         h=input_pts
         for i ,l in enumerate(self.pts_linears):
             h=self.pts_linears[i](h)  # what?
             h=F.relu(h)
             if i in self.skips:
-                h=torch.concat([input_pts,h],-1)
+                h=torch.cat([input_pts,h],-1)
 
         if self.use_viewdirs:
             alpha=self.alpha_linear(h)
             feature=self.feature_linear(h)
-            h=torch.concat([feature,input_views],-1)
+            h=torch.cat([feature,input_views],-1)
 
             for i,l in enumerate(self.view_linears):
                 h=self.view_linears[i](h)
                 h=F.relu(h)
 
             rgb=self.rgb_linear(h)
-            outputs=torch.concat([rgb,alpha],-1)
+            outputs=torch.cat([rgb,alpha],-1)
         else:
             outputs=self.output_linear(h)
+        
+        return outputs
 
     def load_weights_from_keras(self,weights): # use it to load weights and bias of the MLP
         assert self.use_viewdirs,"Not implemented if use_viewdirs=False"
@@ -133,7 +135,7 @@ class NeRF(nn.Module):
         # Load views_linears
         idx_views_linears=2*self.D+2
         self.view_linears[0].weight.data=torch.from_numpy(np.transpose(weights[idx_views_linears]))
-        self.view_linears[0].bias,data=torch.from_numpy(np.transpose(weights[idx_views_linears+1]))
+        self.view_linears[0].bias.data=torch.from_numpy(np.transpose(weights[idx_views_linears+1]))
 
         # Load rgb_linear
         idx_rgb_linear=2*self.D+4
@@ -144,3 +146,89 @@ class NeRF(nn.Module):
         idx_alpha_linear=2*self.D+6
         self.alpha_linear.weight.data=torch.from_numpy(np.transpose(weights[idx_alpha_linear]))
         self.alpha_linear.bias.data=torch.from_numpy(np.transpose(weights[idx_alpha_linear+1]))
+
+
+# Ray helpers
+def get_rays(H,W,K,c2w):
+    i,j=torch.meshgrid(torch.linspace(0,W-1,W),torch.linspace(0,H-1,H)) # pytorch's meshfgrid has indexing = 'ij'
+    i=i.t()
+    j=j.t() # what is the function of t()?
+    dirs=torch.stack([(i-K[0][2])/K[0][0],-(j-K[1][2])/K[1][1],-torch.ones_like(i)],-1)
+    # Rotate ray directions from camera frame to the world frame
+    rays_d=torch.sum(dirs[...,np.newaxis,:] * c2w[:3,:3],-1) # dot product , equals to :[c2w.dot(dir) for dir in dirs]
+    # Translate camera frame's origin to the world frame , it is the origin of all rays
+    rays_o=c2w[:3,-1].expand(rays_d.shape)
+    return rays_o,rays_d
+
+def get_rays_np(H,W,K,c2w):
+    i,j=np.meshgrid(np.arange(W,dtype=np.float32),np.arange(H,dtype=np.float32),indexing='xy')
+    dirs=np.stack([(i-K[0][2])/K[0][0],-(j-K[1][2])/K[1][1],-np.ones_like(i)],-1)
+    # Rotate ray directions from camera frame to the world frame
+    rays_d=np.sum(dirs[...,np.newaxis,:] * c2w[:3,:3],-1) # dot product , equals to :[c2w.dot(dir) for dir in dirs]
+    # Translate camera frame's origin to the world frame , it is the origin of all rays
+    rays_o=np.broadcast_to(c2w[:3,-1],np.shape(rays_d))
+    return rays_o,rays_d
+
+def ndc_rays(H,W,focal,near,rays_o,rays_d):
+    # shift ray origins to near plane
+    t=-(near+rays_o[...,2])/rays_d[...,2]
+    rays_o=rays_o+t[...,None]*rays_d
+
+    # projectiion
+    o0=-1./(W/(2.*focal)) *rays_o[...,0]/rays_o[...,2]
+    o1=-1./(H/(2.*focal)) *rays_o[...,1]/rays_o[...,2]
+    o2=1.+2.*near/rays_o[...,2]
+
+    d0=-1./(W/(2.*focal))*(rays_d[...,0]/rays_d[...,2]-rays_o[...,0]/rays_o[...,2])
+    d1=-1./(H/(2.*focal))*(rays_d[...,1]/rays_d[...,2]-rays_o[...,1]/rays_o[...,2])
+    d2=-2.*near/rays_o[...,2]
+
+    rays_o=torch.stack([o0,o1,o2],-1)
+    rays_d=torch.stack([d0,d1,d2],-1)
+
+    return rays_o,rays_d
+
+def sample_pdf(bins,weights,N_samples,det=False,pytest=False):
+    # get pdf
+    weights=weights + 1e-5 #prevent nans
+    pdf = weights /torch.sum(weights,-1,keepdim=True)
+    cdf = torch.cumsum(pdf,-1)
+    cdf = torch.cat([torch.zeros_like(cdf[...,:1]),cdf],-1) # (batch,len(bins))
+
+    # Take uniform samples
+    if det:
+        u=torch.linspace(0.,1.,steps=N_samples)
+        u=u.expand(list(cdf.shape[:-1])+[N_samples])
+    else:
+        u=torch.rand(list(cdf.shape[:-1])+[N_samples])
+
+    # pytest,overwrite u with numpy's fixed random numbers
+    if pytest:
+        np.random.seed(0)
+        new_shape=list(cdf.shape[:-1])+[N_samples]
+        if det:
+            u=np.linspace(0.,1.,N_samples)
+            u=np.broadcast_to(u,new_shape)
+        else:
+            u=np.random.rand(*new_shape)
+        u=torch.Tensor(u)
+
+    # Invert CDF
+    u=u.contiguous()
+    inds=torch.searchsorted(cdf,u,right=True)
+    below=torch.max(torch.zeros_like(inds-1),inds-1)
+    above=torch.min((cdf.shape[-1]-1)*torch.ones_like(inds),inds)
+    inds_g=torch.stack([below,above],-1) #(batch,N_samples,2)
+
+    matched_shape=[inds_g.shape[0],inds_g.shape[1],cdf.shape[-1]]
+    cdf_g=torch.gather(cdf.unsqueeze(1).expand(matched_shape),2,inds_g)
+    bins_g=torch.gather(bins.unsqueeze(1).expand(matched_shape),2,inds_g)
+
+    denom=(cdf_g[...,1]-cdf_g[...,0])
+    denom=torch.where(denom<1e-5,torch.ones_like(denom),denom)
+    t=(u-cdf_g[...,0])/denom
+    samples=bins_g[...,0]+t*(bins_g[...,1]-bins_g[...,0])
+
+    return samples
+
+
